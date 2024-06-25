bee3d7cddb82ced3461d20e14b1a1b5086dade5e
ankitg841 <ankitg841@gmail.com>
2024-06-25 12:11:44 +0530

    added code for logging in the aop

--- UserService/src/main/java/com/lcwd/user/aspect/LoggingAspect.java
+++ UserService/src/main/java/com/lcwd/user/aspect/LoggingAspect.java
@@ -1,10 +1,9 @@
 package com.lcwd.user.aspect;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
@@ -14,18 +13,41 @@ import org.springframework.stereotype.Component;
 @EnableAspectJAutoProxy
 public class LoggingAspect {
 
-    @Pointcut("execution(* com.lcwd.user.services.UserService.*(..))")
-    public void logBefore()
-    {
-
+    @Pointcut("execution(* com.lcwd.user.*.*.*(..))")
+    public void argumentTypeMethod() {
     }
 
-    @Before("logBefore()")
-    public void logBeforeMethod(JoinPoint joinPoint)
-    {
-        System.out.println("Before methode execution");
-        log.info("Before methode execution : {}",joinPoint.getSignature());
+    @Pointcut("execution(* com.lcwd.user.*.*.*())")
+    public void noArgumentTypeMethod() {
     }
+
+
+
+    @Before("argumentTypeMethod()")
+    public void logBeforeMethodsForArgumentType(JoinPoint joinPoint) {
+        printTheLogs("Before method execution : {}", joinPoint.getSignature().getName());
+    }
+
+    @After("argumentTypeMethod()")
+    public void logAfterMethodsForArgumentType(JoinPoint joinPoint) {
+        printTheLogs("After method execution : {}", joinPoint.getSignature().getName());
+    }
+
+    @Before("argumentTypeMethod()")
+    public void logBeforeMethodsForNoArgumentType(JoinPoint joinPoint) {
+        printTheLogs("Before method execution : {}", joinPoint.getSignature().getName());
+    }
+
+    @After("argumentTypeMethod()")
+    public void logAfterMethodsForNoArgumentType(JoinPoint joinPoint) {
+        printTheLogs("After method execution : {}", joinPoint.getSignature().getName());
+    }
+
+    public void printTheLogs(String str1,String str2)
+    {
+        log.info(str1,str2);
+    }
+
 
 
 }
